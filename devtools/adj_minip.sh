75db509bea50442d05247c9237ce2e6f41316ae6
Trond Trosterud <trond.trosterud@uit.no>
2016-12-22 13:03:00 +0000

    Test suite for generating paradigms, modeled upon Lene's scripts for smn.

---
+++ devtools/adj_minip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# script to generate paradigms for generating word forms
+# command:
+# sh generate_contlex_para.sh PATTERN
+# example, when you are in smn:
+# sh devtools/adj_minip.sh A_  | less
+# sh devtools/adj_minip.sh кугу 
+# Only get the lemma you ask for:
+# sh devtools/adj_minip.sh '^кугу[:+]' 
+
+LOOKUP=$(echo $LOOKUP)
+GTHOME=$(echo $GTHOME)
+
+PATTERN=$1
+L_FILE="in.txt"
+cut -d '!' -f1 src/morphology/stems/adjectives.lexc | egrep $PATTERN | cut -d ':' -f1>$L_FILE
+
+P_FILE="test/data/testadjparadigm.txt"
+
+for lemma in $(cat $L_FILE);
+do
+ for form in $(cat $P_FILE);
+ do
+   echo "${lemma}${form}" | $LOOKUP $GTHOME/langs/smn/src/generator-gt-norm.xfst
+ done
+done
+
